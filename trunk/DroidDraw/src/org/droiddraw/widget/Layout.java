edae2a9565792112026cddb48b5adc0a94c7ccd3
brendan.d.burns <brendan.d.burns@8fca31d7-1c40-0410-b102-91b938957e28>
2008-01-03 01:40:08 +0000

    added the ability to drag and drop between layouts. cleaned up selection a bit

--- trunk/DroidDraw/src/org/droiddraw/widget/Layout.java
+++ trunk/DroidDraw/src/org/droiddraw/widget/Layout.java
@@ -14,6 +14,7 @@ public interface Layout extends Widget {
 	public void addEditableProperties(Widget w);
 	public void removeEditableProperties(Widget w);
 	public void removeAllWidgets();
+	public boolean containsWidget(Widget w);
 	public int getScreenX();
 	public int getScreenY();
 	
