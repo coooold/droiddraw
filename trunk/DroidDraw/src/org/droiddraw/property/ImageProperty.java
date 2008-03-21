0277bca6693cf77c85f33be50995b686e173009d
brendan.d.burns <brendan.d.burns@8fca31d7-1c40-0410-b102-91b938957e28>
2008-03-21 02:24:29 +0000

    fixed layout colors, added browseable image support

---
+++ trunk/DroidDraw/src/org/droiddraw/property/ImageProperty.java
@@ -0,0 +1,7 @@
+package org.droiddraw.property;
+
+public class ImageProperty extends StringProperty {
+	public ImageProperty(String englishName, String attName, String defaultValue) {
+		super(englishName, attName, defaultValue);
+	}
+}
